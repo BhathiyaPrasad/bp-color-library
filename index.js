3b4db21fc77a564a6e6feac263c39cb8100eeedd
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-11-21 12:16:07 +0530

    implement colour finder function

---
+++ index.js
@@ -0,0 +1,9 @@
+import colorNameList from "../src/data/colornames.json";
+
+export function getColorName(hex) {
+  const match = colorNameList.find(color => color.hex.toLowerCase() === hex.toLowerCase());
+  return match ? match.name : "Unknown color";
+}
+
+console.log("this is color function",getColorName("#FF5733")); // Output: Some color name (depends on the library)
+
